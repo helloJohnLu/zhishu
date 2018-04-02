adce5d2c9588ea73aa0df1641ccd3b1fe486e37d
John Lu <johnluchn@gmail.com>
2018-04-02 16:45:18 +0000

    install IDE helper

--- config/app.php
+++ config/app.php
@@ -177,6 +177,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * IDE helper
+         */
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+
     ],
 
     /*
