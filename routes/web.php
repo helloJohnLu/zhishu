2cab01180b0d8d0a9fdbfe1ff799cef8ca54bb8b
John Lu <johnluchn@gmail.com>
2018-04-03 20:10:07 +0000

    用户模块，登录注册与个人设置中心，定义路由

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-// 文章模块
+/* 文章模块 */
 Route::resource('posts', 'PostController');
 // URL跳转
 Route::get('posts', function () {
@@ -13,5 +13,17 @@ Route::get('/', 'PostController@index')->name('posts.index');
 Route::post('posts/image/upload', 'PostController@imageUpload')->name('posts.imageUpload');
 
 
-
+/* 用户模块*/
+// 用户注册
+Route::get('register', 'RegisterController@index')->name('register.index');
+Route::post('register', 'RegisterController@store')->name('register.store');
+// 登录
+Route::get('login', 'LoginController@index')->name('login.index');
+Route::post('login', 'LoginController@store')->name('login.store');
+// 登出
+Route::get('logout', 'LoginController@logout')->name('login.logout');
+// 个人设置页面
+Route::get('user/me/setting', 'UserController@setting')->name('user.setting');
+// 个人设置操作
+Route::post('user/me/setting', 'UserController@settingStore')->name('user.settingStore');
 
