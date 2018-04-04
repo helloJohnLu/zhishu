5c316340be1f7fff13d7ce407ce3f1d398701d3c
John Lu <johnluchn@gmail.com>
2018-04-04 05:39:47 +0000

    微调

--- resources/views/layouts/_sidebar.blade.php
+++ resources/views/layouts/_sidebar.blade.php
@@ -7,10 +7,13 @@
         </div>
         <div class="panel-body">
             <p>
-                欢迎来到 知 · 书 社区
+                欢迎来到 <strong><a href="/">知 · 书 社区</a></strong>
             </p>
             <p>
-                <strong><a href="/">知 · 书 社区</a></strong> 基于 Laravel 5.5 构建
+                知 · 书 致力于成为最优质的内容分享社区。每天，有成千上万的人在这里分享他们的所知、所见、所闻。每一分钟，都有用户通过 知 · 书 创作出新的精彩，并与志同道合的人在这里进行互动交流。
+            </p>
+            <p>
+                分享你的故事，在这里，现在就行动吧！
             </p>
             <div class="bdsharebuttonbox bdshare-button-style0-24" data-bd-bind="1494580268777"><a href="#" class="bds_more" data-cmd="more"></a><a href="#" class="bds_tsina" data-cmd="tsina" title="分享到新浪微博"></a><a href="#" class="bds_renren" data-cmd="renren" title="分享到人人网"></a><a href="#" class="bds_douban" data-cmd="douban" title="分享到豆瓣网"></a><a href="#" class="bds_weixin" data-cmd="weixin" title="分享到微信"></a><a href="#" class="bds_qzone" data-cmd="qzone" title="分享到QQ空间"></a><a href="#" class="bds_tqq" data-cmd="tqq" title="分享到腾讯微博"></a><a href="#" class="bds_bdhome" data-cmd="bdhome" title="分享到百度新首页"></a></div>
             <script>window._bd_share_config={"common":{"bdSnsKey":{"tsina":"120473611"},"bdText":"","bdMini":"2","bdMiniList":false,"bdPic":"","bdStyle":"0","bdSize":"24"},"share":{},"image":{"viewList":["tsina","renren","douban","weixin","qzone","tqq","bdhome"],"viewText":"分享到：","viewSize":"16"},"selectShare":{"bdContainerClass":null,"bdSelectMiniList":["tsina","renren","douban","weixin","qzone","tqq","bdhome"]}};with(document)0[(getElementsByTagName('head')[0]||body).appendChild(createElement('script')).src='http://bdimg.share.baidu.com/static/api/js/share.js?v=89860593.js?cdnversion='+~(-new Date()/36e5)];</script>
