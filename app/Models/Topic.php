5ff59fc21df7ed7bcd1bb06b3eeeba14409a6c2d
John Lu <johnluchn@gmail.com>
2018-04-12 08:41:49 +0000

    专题模块，专题文章模型关联

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Topic extends Model
 {
-    //
+    // 属于某个专题的所有文章    专题 & 文章 多对多
+    public function posts()
+    {
+        $this->belongsToMany(\App\Models\Post::class, 'post_topics', 'topic_id', 'post_id');
+    }
+
+    // 专题的文章数, 用于 withCount
+    public function postTopics()
+    {
+        $this->hasMany(\App\Models\PostTopic::class, 'topic_id');
+    }
 }
