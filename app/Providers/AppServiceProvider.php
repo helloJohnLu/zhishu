0eef2925ecfd104be32393ad620a3b4c8190bc2b
John Lu <johnluchn@gmail.com>
2018-04-12 06:45:38 +0000

    使用视图合成器实现公共区域的专题列表

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Models\Topic;
 use Carbon\Carbon;
 use Illuminate\Support\ServiceProvider;
 
@@ -15,6 +16,12 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Carbon::setLocale('zh');
+        // 视图合成器
+        \View::composer('layouts._sidebar', function ($view) {
+            $topics = Topic::all();
+            $view->with('topics', $topics); // 注入专题模型
+        });
+
     }
 
     /**
