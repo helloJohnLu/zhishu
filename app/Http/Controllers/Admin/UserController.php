cf705c341f094ba6f919d16a32bddfb8d1ca556f
John Lu <johnluchn@gmail.com>
2018-04-13 10:05:17 +0000

    管理人员列表

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Models\AdminUser;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -15,7 +16,8 @@ class UserController extends Controller
     // 管理员列表
     public function index()
     {
-        return view('admin.user.index');
+        $users = AdminUser::paginate(10);
+        return view('admin.user.index', compact('users'));
     }
 
     // 添加管理员页面
