af73124f6ec356932a9cc6530d52ebfabb60d213
John Lu <johnluchn@gmail.com>
2018-04-17 07:05:23 +0000

    视图 Topic 目录改小写 topic

--- database/migrations/2018_04_04_201935_alter_users_table.php
+++ database/migrations/2018_04_04_201935_alter_users_table.php
@@ -14,7 +14,7 @@ class AlterUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('avatar', 150);
+            $table->string('avatar', 150)->nullable()->default('');
         });
     }
 
