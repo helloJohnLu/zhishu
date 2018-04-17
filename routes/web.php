b222a2332efa00954bbfe8f784d3028a27e93636
John Lu <johnluchn@gmail.com>
2018-04-17 03:05:49 +0000

    合并路由

--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,9 @@ Route::get('/posts/search', 'PostController@search')->name('search');
 /* 文章模块 */
 Route::resource('posts', 'PostController');
 // URL跳转
-Route::get('posts', function () {
+/*Route::get('posts', function () {
     return redirect()->route('posts.index');
-});
+});*/
 // 首页
 Route::get('/', 'PostController@index')->name('posts.index');
 // 文章图片上传
@@ -55,4 +55,64 @@ Route::post('topic/{topic}/submit', 'TopicController@submit')->name('topic.submi
 Route::get('notices', 'NoticeController@index');
 
 
-include_once 'admin.php';
+/* 后台路由 */
+
+Route::group(['prefix' => 'admin'], function () {
+    // 登录页面
+    Route::get('login', 'Admin\LoginController@index')->name('adminLogin.index');
+    // 登录提交
+    Route::post('login', 'Admin\LoginController@login')->name('adminLogin.login');
+    // 登出
+    Route::get('logout', 'Admin\LoginController@logout')->name('adminLogin.logout');
+
+
+    // 首页
+    Route::get('home', 'Admin\HomeController@index')->name('adminHome.index');
+
+    /* 角色权限 */
+    Route::group(['middleware' => 'can:system'], function () {
+        // 管理人员模块
+        Route::get('users', 'Admin\UserController@index')->name('adminUser.index');
+        Route::get('users/create', 'Admin\UserController@create')->name('adminUser.create');
+        Route::post('users/store', 'Admin\UserController@store')->name('adminUser.store');
+
+        /* 角色模块 */
+        // 角色列表页
+        Route::get('roles', 'Admin\RoleController@index')->name('role.index');
+        // 角色创建
+        Route::get('roles/create', 'Admin\RoleController@create')->name('role.create');
+        Route::post('roles/store', 'Admin\RoleController@store')->name('role.store');
+
+        // 角色 & 用户 关联
+        Route::get('users/{user}/role', 'Admin\UserController@role')->name('adminUser.role');
+        Route::post('users/{user}/role', 'Admin\UserController@storeRole')->name('adminUser.storeRole');
+        // 权限 & 角色 关联
+        Route::get('roles/{role}/permission', 'Admin\RoleController@permission')->name('role.permission');
+        Route::post('roles/{role}/permission', 'Admin\RoleController@storePermission')->name('role.storePermission');
+
+        /* 权限模块 */
+        // 权限列表页
+        Route::get('permissions', 'Admin\PermissionController@index')->name('permission.index');
+        // 权限创建
+        Route::get('permissions/create', 'Admin\PermissionController@create')->name('permission.create');
+        Route::post('permissions/store', 'Admin\PermissionController@store')->name('permission.store');
+    });
+
+    Route::group(['middleware' => 'can:post'], function () {
+        // 文章管理模块
+        Route::get('posts', 'Admin\PostController@index')->name('adminPost.index');
+        Route::post('posts/{post}/status', 'Admin\PostController@status')->name('adminPost.status');
+    });
+
+
+    /* 专题模块 */
+    Route::group(['middleware' => 'can:topic'], function () {
+        Route::resource('topics', 'Admin\TopicController', ['only' => ['index', 'create', 'store', 'destroy']]);
+    });
+
+
+    /* 通知模块 */
+    Route::group(['middleware' => 'can:notice'], function () {
+        Route::resource('notices', 'Admin\NoticeController', ['only' => ['index', 'create', 'store']]);
+    });
+});
