e094b71fc7513fd6f5aa262d9a9e1bba39908366
John Lu <johnluchn@gmail.com>
2019-07-01 15:32:10 +0800

    edited index.blade.php, add slider images to local

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -12,15 +12,15 @@
                     </ol><!-- Wrapper for slides -->
                     <div class="carousel-inner">
                         <div class="item active">
-                            <img src="http://ww1.sinaimg.cn/large/44287191gw1excbq6tb3rj21400migrz.jpg" alt="..." />
+                            <img src=" {{ asset('image/slider-1.jpg')}} " alt="..." />
                             <div class="carousel-caption">...</div>
                         </div>
                         <div class="item">
-                            <img src="http://ww3.sinaimg.cn/large/44287191gw1excbq5iwm6j21400min3o.jpg" alt="..." />
+                            <img src=" {{ asset('image/slider-2.jpg')}} " alt="..." />
                             <div class="carousel-caption">...</div>
                         </div>
                         <div class="item">
-                            <img src="http://ww2.sinaimg.cn/large/44287191gw1excbq4kx57j21400migs4.jpg" alt="..." />
+                            <img src=" {{ asset('image/slider-3.jpg')}} " alt="..." />
                             <div class="carousel-caption">...</div>
                         </div>
                     </div>
